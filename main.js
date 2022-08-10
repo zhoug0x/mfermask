e4e9e7c93c1be0b04c1fa1a2998e6c9c63f72aa5
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-08-10 18:58:36 -0300

    tweaks and tidying

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
-const DETECT_THRESHOLD = 0.8
+const FACE_DETECT_THRESHOLD = 0.8
 
 function main() {
-	let CVD = null // return of Canvas2DDisplay
+	let canvasHelper
 
+	// options for jeeliz face filter:
+	// https://github.com/jeeliz/jeelizFaceFilter#optional-init-arguments
 	JEELIZFACEFILTER.init({
 		canvasId: 'output-canvas',
 		NNCPath: './dist/', // root directory containing NN_DEFAULT.json neural network model
+		animateDelay: 1,
+
+		// runs on initialization
 		callbackReady: (error, spec) => {
 			if (error) {
 				console.error(error)
 				return
 			}
 
-			console.log('jeelizFaceFilter ready...')
-			CVD = JeelizCanvas2DHelper(spec)
-			CVD.ctx.strokeStyle = 'cyan'
+			console.log('neural net loaded...')
+			canvasHelper = JeelizCanvas2DHelper(spec)
 		},
 
-		// called at each render iteration
-		callbackTrack: function (detectState) {
-			// if face detected, draw border around face
-			if (detectState.detected > DETECT_THRESHOLD) {
-				const faceCoo = CVD.getCoordinates(detectState)
-				CVD.ctx.clearRect(0, 0, CVD.canvas.width, CVD.canvas.height)
-				CVD.ctx.strokeRect(faceCoo.x, faceCoo.y, faceCoo.w, faceCoo.h)
-				CVD.update_canvasTexture()
+		// runs each draw loop iteration
+		callbackTrack: detectState => {
+			// if face detected in the frame
+			if (detectState.detected > FACE_DETECT_THRESHOLD) {
+				
+				// probability if mouth open or not - num between 0 & 1
+				// console.log(detectState.expressions)
+
+				const { getCoordinates, ctx, canvas, update_canvasTexture } =
+					canvasHelper
+
+				// get coordinates of face on screen
+				const faceCoords = getCoordinates(detectState)
+				ctx.strokeStyle = 'cyan'
+				ctx.clearRect(0, 0, canvas.width, canvas.height)
+				ctx.strokeRect(faceCoords.x, faceCoords.y, faceCoords.w, faceCoords.h)
+				update_canvasTexture()
 			}
-			CVD.draw()
+			canvasHelper.draw()
 		},
 	})
 }
