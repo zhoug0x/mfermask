ea9eb4dd45cc93d2db1d34fc82b0fe2502afacb3
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-08-11 16:47:38 -0300

    refactoring main to avoid direct state mutation, housekeeping & comments

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const FACE_DETECT_THRESHOLD = 0.8
 
 function main() {
-	let canvasHelper
+	let _canvasHelper
+
+	const setCanvasHelper = helper => {
+		_canvasHelper = helper
+	}
+
+	const getCanvasHelper = () => _canvasHelper
 
 	// options for jeeliz face filter:
 	// https://github.com/jeeliz/jeelizFaceFilter#optional-init-arguments
 	JEELIZFACEFILTER.init({
 		canvasId: 'output-canvas',
-		NNCPath: './dist/', // root directory containing NN_DEFAULT.json neural network model
+		NNCPath: './jeeliz/', // root directory containing NN_DEFAULT.json neural network model
 		animateDelay: 1,
 
 		// runs on initialization
@@ -18,14 +24,14 @@ function main() {
 			}
 
 			console.log('neural net loaded...')
-			canvasHelper = JeelizCanvas2DHelper(spec)
+			setCanvasHelper(JeelizCanvas2DHelper(spec))
 		},
 
 		// runs each draw loop iteration
 		callbackTrack: detectState => {
+			const canvasHelper = getCanvasHelper()
 			// if face detected in the frame
 			if (detectState.detected > FACE_DETECT_THRESHOLD) {
-				
 				// probability if mouth open or not - num between 0 & 1
 				// console.log(detectState.expressions)
 
@@ -34,9 +40,13 @@ function main() {
 
 				// get coordinates of face on screen
 				const faceCoords = getCoordinates(detectState)
+
+				// build thing to put on the face
 				ctx.strokeStyle = 'cyan'
 				ctx.clearRect(0, 0, canvas.width, canvas.height)
 				ctx.strokeRect(faceCoords.x, faceCoords.y, faceCoords.w, faceCoords.h)
+
+				// flag the canvas helper to trigger a canvas update on the next draw()
 				update_canvasTexture()
 			}
 			canvasHelper.draw()
