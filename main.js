0c7120f73fdd69aec30494496eb24281d05a10d3
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-08-18 13:12:15 -0300

    added imgs, debug display

--- main.js
+++ main.js
@@ -1,14 +1,60 @@
+const DEBUG_MODE = true
 const FACE_DETECT_THRESHOLD = 0.8
-const debugDisplay = document.querySelector('#debug-display')
+const el_HeadImg = document.querySelector('#head-img')
+const el_DebugDisplay = document.querySelector('#debug-display')
+
+const displayDebugData = data => {
+	el_DebugDisplay.innerText = JSON.stringify(data, null, 2)
+}
 
 function main() {
-	let _canvasHelper
+	let canvasHelper
 
-	const setCanvasHelper = helper => {
-		_canvasHelper = helper
+	// runs on initialization
+	const callbackReady = (error, spec) => {
+		if (error) {
+			console.error(error)
+			return
+		}
+
+		canvasHelper = JeelizCanvas2DHelper(spec)
+		console.log('ready!')
 	}
 
-	const getCanvasHelper = () => _canvasHelper
+	// runs each draw loop iteration
+	const callbackTrack = detectState => {
+		// if face detected in the frame
+		if (detectState.detected > FACE_DETECT_THRESHOLD) {
+			const { getCoordinates, ctx, canvas, update_canvasTexture } = canvasHelper
+
+			// get location of face on screen
+			const faceCoords = getCoordinates(detectState)
+
+			// parse detected facial expressions
+			const { expressions: expr } = detectState
+			const expressions = {
+				mouthOpen: expr[0],
+				mouthSmile: expr[1],
+				eyebrowFrown: expr[2],
+				eyebrowRaised: expr[3],
+			}
+
+			if (DEBUG_MODE) {
+				displayDebugData({ faceCoords, expressions })
+
+				ctx.strokeStyle = 'yellow'
+				ctx.clearRect(0, 0, canvas.width, canvas.height)
+				ctx.strokeRect(faceCoords.x, faceCoords.y, faceCoords.w, faceCoords.h)
+			}
+
+			ctx.drawImage(el_HeadImg, faceCoords.x, faceCoords.y, faceCoords.w * 2, faceCoords.h * 2)
+
+			// flag the `canvasHelper` to trigger a canvas update on the next `canvasHelper.draw()`
+			update_canvasTexture()
+		}
+
+		canvasHelper.draw()
+	}
 
 	// options for jeeliz face filter:
 	// https://github.com/jeeliz/jeelizFaceFilter#optional-init-arguments
@@ -16,49 +62,8 @@ function main() {
 		canvasId: 'output-canvas',
 		NNCPath: './jeeliz/NN_4EXPR_1.json', // path to the neural network model json
 		animateDelay: 1,
-
-		// runs on initialization
-		callbackReady: (error, spec) => {
-			if (error) {
-				console.error(error)
-				return
-			}
-
-			console.log('neural net loaded...')
-			setCanvasHelper(JeelizCanvas2DHelper(spec))
-		},
-
-		// runs each draw loop iteration
-		callbackTrack: detectState => {
-			const canvasHelper = getCanvasHelper()
-			// if face detected in the frame
-			if (detectState.detected > FACE_DETECT_THRESHOLD) {
-				const { getCoordinates, ctx, canvas, update_canvasTexture } =
-					canvasHelper
-
-				// get coordinates of face on screen
-				const faceCoords = getCoordinates(detectState)
-
-				const expr = detectState.expressions
-				const mouthOpen = expr[0]
-				const mouthSmile = expr[1]
-				const eyebrowFrown = expr[2]
-				const eyebrowRaised = expr[3]
-
-				// TODO: use expressions
-				console.log({ mouthOpen, mouthSmile, eyebrowFrown, eyebrowRaised })
-
-				// build thing to put on the face
-				ctx.strokeStyle = 'cyan'
-				ctx.clearRect(0, 0, canvas.width, canvas.height)
-				ctx.strokeRect(faceCoords.x, faceCoords.y, faceCoords.w, faceCoords.h)
-
-				// flag the canvas helper to trigger a canvas update on the next draw()
-				update_canvasTexture()
-			}
-
-			canvasHelper.draw()
-		},
+		callbackReady,
+		callbackTrack,
 	})
 }
 
