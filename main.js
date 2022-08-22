9bbd86506f09447d29390dc169125ae9de1703f0
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-08-22 15:03:32 -0300

    moving math helpers to own file, tidying

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-/*
-The code of this demonstration may seems complicated but it is not
-If you want to play only with the 2D canvas displayed above the head,
-Its context is CTX and it matches the canvas element CANVAS2D
-
-If you draw something on the 2D canvas using CTX.drawStuff(...)
-You should call update_canvasTexture(); after in order to update the 3D canvas
-at the next rendering.
-*/
-
-// SETTINGS of this demo:
+// global settings
 const SETTINGS = {
 	strokeStyle: 'red',
 	rotationOffsetX: 0, // negative -> look upper. in radians
@@ -18,10 +8,20 @@ const SETTINGS = {
 	detectionHysteresis: 0.05,
 	scale: [1.75, 1.75], // scale of the 2D canvas along horizontal and vertical 2D axis
 	offsetYZ: [-0.25, -0.2], // offset of the 2D canvas along vertical and depth 3D axis
-	canvasSizePx: 512, // resolution of the 2D canvas in pixels
+	canvasSizePx: 500, // resolution of the 2D canvas in pixels
 }
 
-// some globalz:
+// matrix algebra math utils
+const {
+	create_mat4Identity,
+	set_mat4Position,
+	set_mat4RotationXYZ,
+	inverse_mat4MoveMatrix,
+	multiply_matVec4,
+	get_mat4Pos,
+} = MATRIX_HELPERS
+
+// globals
 let CV = null,
 	CANVAS2D = null,
 	CTX = null,
@@ -43,87 +43,8 @@ let ZPLANE = 0,
 	YPLANE = 0
 let ISDETECTED = false
 
-// callback: launched if a face is detected or lost.
-function detect_callback(isDetected) {
-	if (isDetected) {
-		console.log('INFO in detect_callback(): DETECTED')
-	} else {
-		console.log('INFO in detect_callback(): LOST')
-	}
-}
-
-// ----------- BEGIN MATRIX ALGEBRA FUNCTIONS
-function create_mat4Identity() {
-	return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
-}
-
-// set the position part of a flattened transposed mat4:
-function set_mat4Position(m, x, y, z) {
-	m[12] = x
-	m[13] = y
-	m[14] = z
-}
-
-// set the rotation part of a flattened transposed mat4 - see https://en.wikipedia.org/wiki/Euler_angles
-function set_mat4RotationXYZ(m, rx, ry, rz) {
-	const c1 = Math.cos(rx),
-		s1 = Math.sin(rx),
-		c2 = Math.cos(ry),
-		s2 = Math.sin(ry),
-		c3 = Math.cos(rz),
-		s3 = Math.sin(rz)
-	// first line (not transposed)
-	m[0] = c2 * c3
-	m[4] = -c2 * s3
-	m[8] = s2
-
-	// second line (not transposed)
-	m[1] = c1 * s3 + c3 * s1 * s2
-	m[5] = c1 * c3 - s1 * s2 * s3
-	m[9] = -c2 * s1
-
-	// third line (not transposed)
-	m[2] = s1 * s3 - c1 * c3 * s2
-	m[6] = c3 * s1 + c1 * s2 * s3
-	m[10] = c1 * c2
-}
-
-// inverse a mat4 move matrix m and put result to mat4 matrix r
-function inverse_mat4MoveMatrix(m, r) {
-	// rotation part: the inverse = the transpose
-	r[0] = m[0]
-	r[1] = m[4]
-	r[2] = m[8]
-
-	r[4] = m[1]
-	r[5] = m[5]
-	r[6] = m[9]
-
-	r[8] = m[2]
-	r[9] = m[6]
-	r[10] = m[10]
-
-	// translation part: = -tR.T where T=[m[12], m[13], m[14]]
-	r[12] = -(m[0] * m[12] + m[1] * m[13] + m[2] * m[14])
-	r[13] = -(m[4] * m[12] + m[5] * m[13] + m[6] * m[14])
-	r[14] = -(m[8] * m[12] + m[9] * m[13] + m[10] * m[14])
-}
-
-function multiply_matVec4(m, v) {
-	return [
-		m[0] * v[0] + m[4] * v[1] + m[8] * v[2] + m[12] * v[3],
-		m[1] * v[0] + m[5] * v[1] + m[9] * v[2] + m[13] * v[3],
-		m[2] * v[0] + m[6] * v[1] + m[10] * v[2] + m[14] * v[3],
-		m[3] * v[0] + m[7] * v[1] + m[11] * v[2] + m[15] * v[3],
-	]
-}
-
-function get_mat4Pos(m) {
-	return [m[12], m[13], m[14]]
-}
-// ----------- END MATRIX ALGEBRA FUNCTIONS
-
 // ------------------- BEGIN WEBGL HELPERS
+// TODO: these functions mutate the GL global, but move them to helpers.js file somehow
 // compile a shader:
 function compile_shader(source, glType, typeString) {
 	const glShader = GL.createShader(glType)
@@ -176,9 +97,7 @@ function update_projMatrix() {
 
 	GL.uniformMatrix4fv(SHADERCANVAS.projMatrix, false, PROJMATRIX)
 	PROJMATRIXNEEDSUPDATE = false
-}
-
-// -------------------- END WEBGL HELPERS
+} // -------------------- END WEBGL HELPERS
 
 //build the 3D. called once when Jeeliz Face Filter is OK
 function init_scene(spec) {
@@ -200,7 +119,7 @@ function init_scene(spec) {
 
 	const headImage = new Image()
 	headImage.src = 'images/plain-head.png'
-	headImage.onload = function () {
+	headImage.onload = () => {
 		CTX.drawImage(
 			headImage,
 			0,
@@ -331,117 +250,117 @@ function update_canvasTexture() {
 	CANVASTEXTURENEEDSUPDATE = true
 }
 
-// entry point - launched by body.onload():
-function main() {
+// runs on facefilter initialization
+const callbackReady = (error, spec) => {
+	if (error) {
+		console.error(error)
+		return
+	}
+
+	console.log('ready...')
+	init_scene(spec)
+}
+
+// runs on each facefilter draw loop iteration
+const callbackTrack = detectState => {
+	if (
+		ISDETECTED &&
+		detectState.detected <
+			SETTINGS.detectionThreshold - SETTINGS.detectionHysteresis
+	) {
+		// DETECTION LOST
+		ISDETECTED = false
+	} else if (
+		!ISDETECTED &&
+		detectState.detected >
+			SETTINGS.detectionThreshold + SETTINGS.detectionHysteresis
+	) {
+		// FACE DETECTED
+		ISDETECTED = true
+	}
+
+	// render the video screen:
+	GL.viewport(0, 0, CV.width, CV.height)
+	GL.useProgram(SHADERVIDEO.program)
+	GL.uniformMatrix2fv(SHADERVIDEO.videoTransformMat2, false, VIDEOTRANSFORMMAT2)
+	GL.bindTexture(GL.TEXTURE_2D, VIDEOTEXTURE)
+	GL.drawElements(GL.TRIANGLES, 3, GL.UNSIGNED_SHORT, 0)
+
+	if (ISDETECTED) {
+		const aspect = CV.width / CV.height
+
+		// move the cube in order to fit the head:
+		const tanFOV = Math.tan((aspect * SETTINGS.cameraFOV * Math.PI) / 360) // tan(FOV/2), in radians
+		const W = detectState.s // relative width of the detection window (1-> whole width of the detection window)
+		const D = 1 / (2 * W * tanFOV) // distance between the front face of the cube and the camera
+
+		// coords in 2D of the center of the detection window in the viewport:
+		const xv = detectState.x
+		const yv = detectState.y
+
+		// coords in 3D of the center of the cube (in the view coordinates system):
+		const z = -D - 0.5 // minus because view coordinate system Z goes backward. -0.5 because z is the coord of the center of the cube (not the front face)
+		const x = xv * D * tanFOV
+		const y = (yv * D * tanFOV) / aspect
+
+		// move and rotate the cube:
+		set_mat4Position(
+			MOVMATRIX,
+			x,
+			y + SETTINGS.pivotOffsetYZ[0],
+			z + SETTINGS.pivotOffsetYZ[1]
+		)
+		set_mat4RotationXYZ(
+			MOVMATRIX,
+			detectState.rx + SETTINGS.rotationOffsetX,
+			detectState.ry,
+			detectState.rz
+		)
+
+		// render the canvas above:
+		GL.clear(GL.DEPTH_BUFFER_BIT)
+		GL.enable(GL.BLEND)
+		GL.blendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA)
+		GL.useProgram(SHADERCANVAS.program)
+		GL.enableVertexAttribArray(SHADERCANVAS.position)
+		GL.enableVertexAttribArray(SHADERCANVAS.uv)
+		GL.uniformMatrix4fv(SHADERCANVAS.movMatrix, false, MOVMATRIX)
+		if (PROJMATRIXNEEDSUPDATE) {
+			update_projMatrix()
+		}
+		GL.bindTexture(GL.TEXTURE_2D, CANVASTEXTURE)
+		if (CANVASTEXTURENEEDSUPDATE) {
+			GL.texImage2D(
+				GL.TEXTURE_2D,
+				0,
+				GL.RGBA,
+				GL.RGBA,
+				GL.UNSIGNED_BYTE,
+				CANVAS2D
+			)
+			CANVASTEXTURENEEDSUPDATE = false
+		}
+		GL.bindBuffer(GL.ARRAY_BUFFER, VBO_VERTEX)
+		GL.vertexAttribPointer(SHADERCANVAS.position, 3, GL.FLOAT, false, 20, 0)
+		GL.vertexAttribPointer(SHADERCANVAS.uv, 2, GL.FLOAT, false, 20, 12)
+		GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, VBO_FACES)
+		GL.drawElements(GL.TRIANGLES, 6, GL.UNSIGNED_SHORT, 0)
+		GL.disableVertexAttribArray(SHADERCANVAS.uv)
+		GL.disableVertexAttribArray(SHADERCANVAS.position)
+		GL.disable(GL.BLEND)
+	}
+}
+
+// ~*~*~*~ the entry point ~*~*~*~
+const main = () => {
+	// initialize jeeliz face filter
+	// options: https://github.com/jeeliz/jeelizFaceFilter#optional-init-arguments
 	JEELIZFACEFILTER.init({
 		canvasId: 'output-canvas',
 		NNCPath: './lib/NN_4EXPR_1.json',
-		callbackReady: function (errCode, spec) {
-			if (errCode) {
-				console.error(errCode)
-				return
-			}
-
-			console.log('facefilter ready!')
-			init_scene(spec)
-		}, //end callbackReady()
-
-		// called at each render iteration (drawing loop):
-		callbackTrack: function (detectState) {
-			if (
-				ISDETECTED &&
-				detectState.detected <
-					SETTINGS.detectionThreshold - SETTINGS.detectionHysteresis
-			) {
-				// DETECTION LOST
-				detect_callback(false)
-				ISDETECTED = false
-			} else if (
-				!ISDETECTED &&
-				detectState.detected >
-					SETTINGS.detectionThreshold + SETTINGS.detectionHysteresis
-			) {
-				// FACE DETECTED
-				detect_callback(true)
-				ISDETECTED = true
-			}
-
-			// render the video screen:
-			GL.viewport(0, 0, CV.width, CV.height)
-			GL.useProgram(SHADERVIDEO.program)
-			GL.uniformMatrix2fv(
-				SHADERVIDEO.videoTransformMat2,
-				false,
-				VIDEOTRANSFORMMAT2
-			)
-			GL.bindTexture(GL.TEXTURE_2D, VIDEOTEXTURE)
-			GL.drawElements(GL.TRIANGLES, 3, GL.UNSIGNED_SHORT, 0)
-
-			if (ISDETECTED) {
-				const aspect = CV.width / CV.height
-
-				// move the cube in order to fit the head:
-				const tanFOV = Math.tan((aspect * SETTINGS.cameraFOV * Math.PI) / 360) // tan(FOV/2), in radians
-				const W = detectState.s // relative width of the detection window (1-> whole width of the detection window)
-				const D = 1 / (2 * W * tanFOV) // distance between the front face of the cube and the camera
-
-				// coords in 2D of the center of the detection window in the viewport:
-				const xv = detectState.x
-				const yv = detectState.y
-
-				// coords in 3D of the center of the cube (in the view coordinates system):
-				const z = -D - 0.5 // minus because view coordinate system Z goes backward. -0.5 because z is the coord of the center of the cube (not the front face)
-				const x = xv * D * tanFOV
-				const y = (yv * D * tanFOV) / aspect
-
-				// move and rotate the cube:
-				set_mat4Position(
-					MOVMATRIX,
-					x,
-					y + SETTINGS.pivotOffsetYZ[0],
-					z + SETTINGS.pivotOffsetYZ[1]
-				)
-				set_mat4RotationXYZ(
-					MOVMATRIX,
-					detectState.rx + SETTINGS.rotationOffsetX,
-					detectState.ry,
-					detectState.rz
-				)
-
-				// render the canvas above:
-				GL.clear(GL.DEPTH_BUFFER_BIT)
-				GL.enable(GL.BLEND)
-				GL.blendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA)
-				GL.useProgram(SHADERCANVAS.program)
-				GL.enableVertexAttribArray(SHADERCANVAS.position)
-				GL.enableVertexAttribArray(SHADERCANVAS.uv)
-				GL.uniformMatrix4fv(SHADERCANVAS.movMatrix, false, MOVMATRIX)
-				if (PROJMATRIXNEEDSUPDATE) {
-					update_projMatrix()
-				}
-				GL.bindTexture(GL.TEXTURE_2D, CANVASTEXTURE)
-				if (CANVASTEXTURENEEDSUPDATE) {
-					GL.texImage2D(
-						GL.TEXTURE_2D,
-						0,
-						GL.RGBA,
-						GL.RGBA,
-						GL.UNSIGNED_BYTE,
-						CANVAS2D
-					)
-					CANVASTEXTURENEEDSUPDATE = false
-				}
-				GL.bindBuffer(GL.ARRAY_BUFFER, VBO_VERTEX)
-				GL.vertexAttribPointer(SHADERCANVAS.position, 3, GL.FLOAT, false, 20, 0)
-				GL.vertexAttribPointer(SHADERCANVAS.uv, 2, GL.FLOAT, false, 20, 12)
-				GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, VBO_FACES)
-				GL.drawElements(GL.TRIANGLES, 6, GL.UNSIGNED_SHORT, 0)
-				GL.disableVertexAttribArray(SHADERCANVAS.uv)
-				GL.disableVertexAttribArray(SHADERCANVAS.position)
-				GL.disable(GL.BLEND)
-			} //end if face detected
-		}, //end callbackTrack()
-	}) //end JEELIZFACEFILTER.init call
+		callbackReady,
+		callbackTrack,
+	})
 }
 
 window.addEventListener('load', main)
